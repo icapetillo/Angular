d6d4eb69733050adb99bd6550947f1ebaca5b79f
icapetillo <israel.capetillo@gmail.com>
2019-03-13 13:28:45 -0600

    Add routes

--- 02-spa/src/app/app.routes.ts
+++ 02-spa/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
     { path: '**', component: HomeComponent }
 ];
 
